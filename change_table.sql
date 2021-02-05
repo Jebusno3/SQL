fe47f507f058f931fa4d3a25805e4ce2fd94de96
Jebusno3 <66726255+Jebusno3@users.noreply.github.com>
2021-02-05 10:50:59 +0000

    Create change_table.sql

---
+++ change_table.sql
@@ -0,0 +1,23 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `change_table`(IN data JSON)
+BEGIN
+DECLARE counter INT DEFAULT 0;
+
+SET @json = data;
+SET @indx = 0;
+SET @keys = JSON_KEYS(@json);
+
+IF (JSON_VALID(@json) = 1) THEN
+WHILE counter < JSON_LENGTH(@json) DO
+ 
+INSERT INTO table_1(`key`, `value`)
+VALUES (JSON_EXTRACT(@keys, CONCAT("$[", @indx, "]")), JSON_EXTRACT(@json, CONCAT("$.", JSON_EXTRACT(@keys, CONCAT("$[", @indx, "]")))));
+SET counter = counter + 1;
+SET @indx = @indx + 1;
+
+END WHILE;
+END IF;
+IF (JSON_VALID(@json) = 0) THEN
+SIGNAL SQLSTATE '45000' SET MYSQL_ERRNO=333, MESSAGE_TEXT='Invalid JSON object';
+END IF;
+
+END
